4b69b33530e766d39bfe1d152b629836a8036c49
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-14 14:19:03 -0700

    update methods, add multiple plants
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function(){
+  $('#feed').click(function() {
+    const newState = stateChanger(blueFood);
+    $('#soil-value').text(newState.soil);
+  });
+});
--- src/plant.js
+++ src/plant.js
@@ -1,30 +1,61 @@
-const storeState = () => {
-    let currentState = {};
-    return (stateChangeFunction) => {
-        const newState = stateChangeFunction(currentState);
-        currentState = {...newState};
-        return newState;
-    }
+const initialState = { soil: 0, water: 0, light: 0};
+
+const storeState = (initialState) => {
+  let currentState = initialState;
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
 }
 
-const stateChanger = storeState();
+const stateChanger = storeState(initialState);
+const fern = storeState(initialState);
+const daffodil = storeState(initialState);
+const rubberPlant = storeState(initialState);
 
 const changeState = (prop) => {
-    return (value) => {
-        return (state) => ({
-            ...state,
-            [prop] : (state[prop] || 0) + value
-        })
-    }
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
 }
+
+// npm install webpack-cli -g
+
+
 
 const feed = changeState("soil");
 const hydrate = changeState("water");
 const giveLight = changeState("light");
 
-const blueFood = feed(5)
-const greenFood = changeState("soil")(10)
-const yuckyFood = changeState("soil")(-5)
+const blueFood = feed(5);
+const greenFood = changeState("soil")(10);
+const yuckyFood = feed(-5);
+
+
+const fedPlant = stateChanger(blueFood);
+// console.log("fedPlant", fedPlant);
+const fedPlantAgain = stateChanger(yuckyFood);
+// console.log("FedPlantAgain", fedPlantAgain);
+
+const fedDaf = daffodil(greenFood);
+console.log("fedDaf", fedDaf);
+const fedFern = fern(yuckyFood);
+console.log("fedFern", fedFern);
+const fedRubber = rubberPlant(blueFood); 
+console.log("fedRubber", fedRubber);
+
+// const plantOne = setPlant(???)(???)("plantOne")
+
+// $(document).ready(function(){
+//   $('#feed').click(function() {
+//     const newState = stateChanger(blueFood);
+//     $('#soil-value').text(newState.soil);
+//   });
+// });
 
 
 
