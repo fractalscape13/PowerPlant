74e9db193523bc7d24613fd7f11ddeeb5fa9e4da
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-14 14:18:42 -0700

    update entry point
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/plant.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -15,10 +14,9 @@ module.exports = {
   },
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
-    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: '',
+      title: 'PowerPlant',
       template: './src/index.html',
       inject: 'body'
     })
